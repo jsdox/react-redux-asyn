d9000de118c1085df5106e8846b47e8fcafe6aa7
jsdox <jatindersingh@founderandlightning.com>
2024-12-05 11:56:37 +0530

    add dummy products to cark

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,30 +1,40 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
-// const DUMMY_PRODUCTS = [
-//   {
-//     id: 'p1',
-//     price: 6,
-//     title: 'My first book',
-//     description: 'My first book description.';
-//   },
-//   {
-//     id: 'p2',
-//     price: 5,
-//     title: 'My second book',
-//     description: 'My second book description.';
-//   }
-// ];
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My first book',
+    description: 'My first book description.'
+  },
+  {
+    id: 'p2',
+    price: 5,
+    title: 'My second book',
+    description: 'My second book description.'
+  }
+];
 const Products = (props) => {
+
+  const renderProducts = () => {
+    return (
+      DUMMY_PRODUCTS.map((product) =>  (      
+        <ProductItem
+          key={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+          id={product.id}
+        />
+    ))
+    );
+  }
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
-      <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+      <ul>     
+        {renderProducts()}         
       </ul>
     </section>
   );
