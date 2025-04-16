e5f2a89d9639e029aee7bc2899ed5f2a21779e17
Jatinder Singh <j.jatinders@gmail.com>
2025-04-16 13:38:51 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
-
+//loading app
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible);
