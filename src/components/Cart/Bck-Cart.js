4b0e5e62b3444a49557051fbda56a009c5975baa
Jatinder Singh <j.jatinders@gmail.com>
2025-05-09 12:31:45 +0530

    Create Bck-Cart.js

---
+++ src/components/Cart/Bck-Cart.js
@@ -0,0 +1,31 @@
+import { useSelector } from 'react-redux';
+import Card from '../UI/Card';
+import classes from './Cart.module.css';
+import CartItem from './CartItem';
+
+const Cart = (props) => {
+  const cartItem = useSelector(state => state.cart.items);
+  // console.log(cartItem);
+  return (
+    <Card className={classes.cart}>
+      <h2>Your Shopping Cart</h2>
+      <ul>
+        {cartItem.map((item) => (
+          <CartItem
+            key={item.id}            
+            item={{ 
+              id:item.id,
+              title: item.title, 
+              quantity: item.quantity, 
+              total: item.totalPrice, 
+              price: item.price 
+            }}
+          />
+        ))}
+        
+      </ul>
+    </Card>
+  );
+};
+
+export default Cart;
