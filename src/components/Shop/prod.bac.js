d369a8675b3f6b80b09e1166b0c8dabac7d8efab
Jatinder Singh <j.jatinders@gmail.com>
2025-06-13 12:07:13 +0530

    Create prod.bac.js

---
+++ src/components/Shop/prod.bac.js
@@ -0,0 +1,43 @@
+import ProductItem from './ProductItem';
+import classes from './Products.module.css';
+
+const DUMMY_PRODUCTS = [
+  {
+    id: 1,
+    price: 6,
+    title: 'My first book',
+    description: 'My first book description.'
+  },
+  {
+    id: 2,
+    price: 5,
+    title: 'My second book',
+    description: 'My second book description.'
+  }
+];
+const Products = (props) => {
+
+  const renderProducts = () => {
+    return (
+      DUMMY_PRODUCTS.map((product) =>  (      
+        <ProductItem
+          key={product.id}
+          title={product.title}
+          price={product.price}
+          description={product.description}
+          id={product.id}
+        />
+    ))
+    );
+  }
+  return (
+    <section className={classes.products}>
+      <h2>Buy your favorite products</h2>
+      <ul>     
+        {renderProducts()}         
+      </ul>
+    </section>
+  );
+};
+
+export default Products;
